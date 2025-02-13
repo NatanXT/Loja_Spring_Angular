5e57331f1f54e8c59bcf9501f662cc9079dca717
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2025-02-13 17:36:43 -0300

    Routing
    Acrescentado esse lazy-loading
    {path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(c => c.CursosModule)}

--- crud-angular-spring/crud-angular/src/app/app.routes.ts
+++ crud-angular-spring/crud-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { CursosModule } from './cursos/cursos.module';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: 'cursos'},
+  {path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(c => c.CursosModule)}
+];
+
