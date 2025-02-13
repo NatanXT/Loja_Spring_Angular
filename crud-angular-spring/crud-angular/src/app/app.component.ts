f863a457920271ad4f1e387472c4cc1463540d55
Natan Derick <114193508+NatanXT@users.noreply.github.com>
2025-02-13 16:46:42 -0300

    Toolbar MatMaterial
    Toolbar via MatMaterial, acrescentada

--- crud-angular-spring/crud-angular/src/app/app.component.ts
+++ crud-angular-spring/crud-angular/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class AppComponent {
   title = 'crud-angular';
